d7038b3e63cc2d69e87fa5da8fbb7a570f24b247
Kristián Kunc <kristian.kunc@gmail.com>
2025-01-15 19:18:11 +0100

    improve entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-set -e
+/usr/sbin/crond -f -l 2 &
 
-/usr/sbin/crond -f -l 8 &
-
-./app/entrypoint.sh &
+./app/backup.sh
 
 exec tail -f /var/log/cron.log /var/log/syslog 2>/dev/null || true
