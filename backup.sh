c196ee2d868ae2cd1d9dd77d5b8e442495850720
Kristián Kunc <kristian.kunc@gmail.com>
2025-01-16 15:36:41 +0100

    fix syntax

--- backup.sh
+++ backup.sh
@@ -17,7 +17,9 @@ echo "Dump uploaded"
 
 gsutil ls gs://$BACKUP_BUCKET/ | while read -r file; do
     file=$(echo "$file" | tr -d '\r\n')
-    if [[ ! "$file" =~ \.sql$ ]]; continue; fi
+    if [[ ! "$file" =~ \.sql$ ]]; then
+        continue
+    fi
 
     filename=$(basename "$file" .sql)
     if [[ $filename =~ ^[0-9]{2}-[0-9]{2}-[0-9]{4}$ ]]; then
@@ -26,7 +28,7 @@ gsutil ls gs://$BACKUP_BUCKET/ | while read -r file; do
             echo "Error parsing date from filename: $filename"
             continue
         fi
-        
+
         current_date=$(date +%s)
         age_days=$(( (current_date - file_date) / 86400 ))
         if [ $age_days -gt $BACKUP_RETENTION_DAYS ]; then
